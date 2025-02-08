abf9adf643538af7554d04ee24a63c3f73a5c8a6
vector17002 <vector17002>
2025-02-08 13:34:56 +0530

    resolved lint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Users, UserPlus, LineChart } from 'lucide-react';
 import Banner from '@/components/Banner';
-import FeatureCard from '@/components/FeatureCard';
 
 export default function LandingPage() {
   return (
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import { UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
 
