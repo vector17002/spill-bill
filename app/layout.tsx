390a546a5874858ee6d14bc8172485cf28e5aac1
vector17002 <vector17002>
2025-02-08 13:46:36 +0530

    added new name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 const montserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Split',
+  title: 'Spill Bill',
   description: 'Split your expenses with your friends',
 };
 
